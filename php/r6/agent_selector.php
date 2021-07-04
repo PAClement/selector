3688724e18a8d16332b3cbeae1da561ea22c991a
clement <clementpaquentin@hotmail.fr>
2021-07-04 10:29:28 +0200

    add commentary

--- php/r6/agent_selector.php
+++ php/r6/agent_selector.php
@@ -2,9 +2,9 @@
 
 	session_start();
 
-	ini_set('display_errors', 1);
+	/*ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+    error_reporting(E_ALL);*/
 
 	include('../CBDD.php');
     $db = new Connection();
